07507ae3844f7708519c46a24cc5b597a4427461
David <david@gul.es>
2013-08-17 13:20:07 +0200

    Renaming test class

--- src/test/scala/org/voiser/zoe/test/MessagesTest.scala
+++ src/test/scala/org/voiser/zoe/test/MessagesTest.scala
@@ -7,9 +7,9 @@ import org.voiser.zoe.MessageParser
 import org.voiser.zoe.MessageBuilder
 
 @RunWith(classOf[JUnitRunner])
-class Messages extends FunSuite {
+class MessagesTest extends FunSuite {
 
-  trait TestMessages {
+  trait Fixtures {
     val m1 = "a=a1&a=a2&b=b1&c==c1="
     val mp1 = new MessageParser(m1)
     val mb = new MessageBuilder().
@@ -17,10 +17,12 @@ class Messages extends FunSuite {
         add("a", "a2").
         add("b", "b1").
         add("c", "=c1=")
+    val m2 = mb.msg
+    val mp2 = new MessageParser(m2)
   }
   
   test("Simple parsing") {
-    new TestMessages {
+    new Fixtures {
       assert(mp1.list("a") === Some(List("a1", "a2")))
       assert(mp1.get("b") === Some("b1"))
       assert(mp1.get("c") === Some("=c1="))
@@ -28,9 +30,7 @@ class Messages extends FunSuite {
   }
   
   test("Simple building") {
-    new TestMessages {
-      val m2 = mb.msg
-      val mp2 = new MessageParser(m2)
+    new Fixtures {
       assert(mp2.list("a") === Some(List("a2", "a1")))
       assert(mp2.get("b") === Some("b1"))
       assert(mp2.get("c") === Some("=c1="))
